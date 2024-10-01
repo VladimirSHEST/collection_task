3ca3939ace7e00510f717875fce472c07fae9b81
Vladimir11 <piterskiyvv@mail.ru>
2024-10-01 22:56:35 +0400

    практическое задание на коллекции

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.HashMap;
+import java.util.Map;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static String[] elements = {"apple", "banana", "apple", "orange", "banana", "apple"};
+
+    public static void main(String[] args) {
+        System.out.println(fillingMap(elements));
+    }
+
+    public static Map<String,Integer> fillingMap(String[] el){
+        Map<String,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < el.length; i++) {
+            map.put(el[i], map.getOrDefault(el[i], 0) + 1);
         }
+        return map;
     }
 }
